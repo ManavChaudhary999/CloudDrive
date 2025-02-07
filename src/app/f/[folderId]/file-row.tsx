5bd195a14df25636d55da31fbea22aced0529fd6
ManavChaudhary999 <999manavchaudhary@gmail.com>
2025-02-07 18:38:52 +0530

    Navigation and Breadcrumbs are fixed

--- src/app/f/[folderId]/file-row.tsx
+++ src/app/f/[folderId]/file-row.tsx
@@ -1,24 +1,24 @@
 import { Folder, FileIcon } from "lucide-react"
+import Link from "next/link";
 import type { DB_FileType, DB_FolderType } from "~/server/db/schema";
 
 
 export function FolderRow(props: {
     folder: DB_FolderType;
-    handleFolderClick: (folderId: number) => void;
 }) {
-    const { folder, handleFolderClick } = props;
+    const { folder } = props;
     
     return (
         <li key={folder.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
         <div className="grid grid-cols-12 gap-4 items-center">
             <div className="col-span-6 flex items-center">
-                <button
-                    onClick={() => handleFolderClick(folder.id)}
+                <Link
+                    href={`/f/${folder.id}`}
                     className="flex items-center text-gray-100 hover:text-blue-400"
                 >
-                <Folder className="mr-3" size={20} />
-                {folder.name}
-                </button>
+                    <Folder className="mr-3" size={20} />
+                    {folder.name}
+                </Link>
             </div>
             <div className="col-span-3 text-gray-400">{"Folder"}</div>
             <div className="col-span-3 text-gray-400">{"--"}</div>
--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -3,6 +3,27 @@ import { files_table, folders_table } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 import DriveContent from "./drive-content";
 
+async function getAllParentsForFolder(folderId: number) {
+    const parents = [];
+    let currentId: number | null = folderId;
+    while (currentId !== null) {
+      const folder = await db
+        .selectDistinct()
+        .from(folders_table)
+        .where(eq(folders_table.id, currentId));
+
+      if (!folder[0]) {
+        // throw new Error("Parent folder not found");
+        currentId = null;
+        return parents;
+      }
+
+      parents.unshift(folder[0]);
+      currentId = folder[0]?.parent;
+    }
+
+    return parents;
+};
 
 export default async function GoogleDriveClone(props: {
     params: Promise<{folderId: string}>
@@ -13,17 +34,21 @@ export default async function GoogleDriveClone(props: {
         return <div>Invalid Folder Id</div>
     }
 
-    const folders = await db
+    const foldersPromise = db
     .select()
     .from(folders_table)
     .where(eq(folders_table.parent, parsedFolderId));
     
-    const files = await db
+    const filesPromise = db
     .select()
     .from(files_table)
     .where(eq(files_table.parent, parsedFolderId));
     
+    const parentsPromise = getAllParentsForFolder(parsedFolderId);
+
+    const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise]);
+
     return (
-        <DriveContent  files={files} folders={folders} />
+        <DriveContent  files={files} folders={folders} parents={parents} />
     );
 }
