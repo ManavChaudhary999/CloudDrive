44b7fc199a4a5b6e559bd4109eec9c971df067fc
ManavChaudhary999 <999manavchaudhary@gmail.com>
2025-02-08 16:13:15 +0530

    Fixed Typeerror in posthog provider

--- src/app/_providers/posthog-provider.ts
+++ src/app/_providers/posthog-provider.ts
@@ -1,10 +1,10 @@
-// "use client";
+"use client";
 
-// import posthog from "posthog-js";
-// import { PostHogProvider as PHProvider } from "posthog-js/react";
-// import { useEffect } from "react";
-// import { env } from "~/env";
-// import dynamicLoader from "next/dynamic";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import { env } from "~/env";
+import dynamicLoader from "next/dynamic";
 
 // const SuspendedPostHogPageView = dynamicLoader(
 //   () => import("./pageview-tracker"),
@@ -13,20 +13,20 @@
 //   },
 // );
 
-// export function PostHogProvider({ children }: { children: React.ReactNode }) {
-//   useEffect(() => {
-//     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-//       api_host: "/ingest",
-//       ui_host: "https://us.posthog.com",
-//       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-//     });
-//   }, []);
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    });
+  }, []);
 
   
-//   return (
-//     <PHProvider client={posthog}>
-//       <SuspendedPostHogPageView />
-//       {children}
-//     </PHProvider>
-//   );
-// }
+  return (
+    // <PHProvider client={posthog}>
+    //   <SuspendedPostHogPageView />
+      {children}
+    // </PHProvider>
+  );
+}
